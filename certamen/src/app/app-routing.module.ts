eed2c8785d9f0c4a035d87e5f8aff1a2c9ea3132
vctraranda <victorarandabarrios@gmail.com>
2022-11-07 20:44:24 +0100

    base de angular hecha

--- certamen/src/app/app-routing.module.ts
+++ certamen/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BandasComponent } from './bandas/bandas.component';
+import { CreditsComponent } from './credits/credits.component';
+import { Error404Component } from './error404/error404.component';
+import { JuecesComponent } from './jueces/jueces.component';
+import { PublicoComponent } from './publico/publico.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"publico", component: PublicoComponent},
+  {path:"bandas", component: BandasComponent },
+  {path:"jueces", component: JuecesComponent },
+  {path:"credits", component: CreditsComponent },
+  {path:"", redirectTo: "/publico", pathMatch:"full" },
+  {path:"**", component: Error404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- certamen/src/app/app.module.ts
+++ certamen/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Component } from './error404/error404.component';
+import { PublicoComponent } from './publico/publico.component';
+import { BandasComponent } from './bandas/bandas.component';
+import { JuecesComponent } from './jueces/jueces.component';
+import { CreditsComponent } from './credits/credits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    FooterComponent,
+    NavBarComponent,
+    Error404Component,
+    PublicoComponent,
+    BandasComponent,
+    JuecesComponent,
+    CreditsComponent
   ],
   imports: [
     BrowserModule,
